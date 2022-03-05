1729b6737915197d58594fd3cf16cf4b08220487
jv3ga <joel.v3ga@gmail.com>
2022-03-05 22:59:36 +0100

    feat: algunas pruebas

--- api.py
+++ api.py
@@ -1,4 +1,24 @@
 import requests
-url = 'https://data.messari.io/api/v1/assets/btc/metrics'
-response = requests.request('GET', url)
-print(response.text)
+import json
+
+url = 'https://data.messari.io/api/v1/assets?fields=id,name,slug,symbol,metrics/market_data/price_usd'
+data = requests.request('GET', url)
+json_string = json.dumps(data.text)
+json_object = json.loads(json_string)
+print(json_object)
+# menu = -1
+menu = 0
+while menu != 0:
+    print('Seleccione una opcion: ')
+    print('1 - Convertir precio de criptomoneda')
+    print('2 - Buscar criptomoneda')
+    print('0 - Salir')
+    menu = int(input('Opción seleccionada: '))
+    if menu == 1:
+        print('Introduce la moneda que quieras convertir')
+    elif menu == 2:
+        print('2')
+    elif menu == 0:
+        print('Adiós')
+    else:
+        print('Opción no disponible')
