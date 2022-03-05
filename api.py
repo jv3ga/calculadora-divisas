23c569d621c7fbf8b3d5b828a1da468a7653e799
jv3ga <joel.v3ga@gmail.com>
2022-03-05 23:12:31 +0100

    fix: funcionando el conseguir el precio

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-url = 'https://data.messari.io/api/v1/assets?fields=id,name,slug,symbol,metrics/market_data/price_usd'
+# url = 'https://data.messari.io/api/v1/assets?fields=id,name,slug,symbol,metrics/market_data/price_usd'
+asset = 'BTC'
+url = 'https://data.messari.io/api/v1/assets/{}/metrics'.format(asset)
 data = requests.request('GET', url)
-json_string = json.dumps(data.text)
-json_object = json.loads(json_string)
-print(json_object)
+print(data.json()['data']['market_data']['price_usd'])
 # menu = -1
 menu = 0
 while menu != 0:
