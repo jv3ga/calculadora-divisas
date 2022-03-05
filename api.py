47bffdcde71e7ecbb007cb344e74161b0be9ed0e
jv3ga <joel.v3ga@gmail.com>
2022-03-06 00:00:47 +0100

    feat: busqueda y multiplicacion

--- api.py
+++ api.py
@@ -1,38 +1,72 @@
 import requests
 
+def is_int(val):
+    try:
+        num = int(val)
+    except ValueError:
+        return False
+    return True
+
 menu = -1
 while menu != 0:
     print('Seleccione una opcion: ')
     print('1 - Convertir precio de criptomoneda')
-    print('2 - Buscar criptomoneda')
+    print('2 - Mostar listado de criptomonedas')
     print('0 - Salir')
-    menu = int(input('Opción seleccionada: '))
-    if menu == 1:
-        asset = input('Introduce la moneda que quieras convertir: ').upper()
-        url = 'https://data.messari.io/api/v1/assets/{}/metrics'.format(asset)
-        data = requests.request('GET', url)
-        price_usd = data.json()['data']['market_data']['price_usd']
-        price_btc = data.json()['data']['market_data']['price_btc']
-        print('El precio en BTC es de {btc}, y en USD {usd}'.format(
-            btc=price_btc,
-            usd=price_usd
-        ))
-        cantidad = int(input('Introduce la cantidad que deseas convertir: '))
-        total_btc = cantidad * price_btc
-        total_usd = cantidad * price_usd
-        print('{cantidad} {asset} a BTC = {total_btc}'.format(
-            cantidad=cantidad,
-            asset=asset,
-            total_btc=total_btc
-        ))
-        print('{cantidad} {asset} a USD = {total_usd}'.format(
-            cantidad=cantidad,
-            asset=asset,
-            total_usd=total_usd
-        ))
-    elif menu == 2:
-        print('2')
-    elif menu == 0:
-        print('Adiós')
+    menu = input('Opción seleccionada: ')
+    if not is_int(menu):
+        print('Selecciona una opción numérica')
     else:
-        print('Opción no disponible')
+        menu = int(menu)
+        if menu == 1:
+            asset = input('Introduce la moneda que quieras convertir: ').upper()
+            url = 'https://data.messari.io/api/v1/assets/{}/metrics'.format(asset)
+            data = requests.request('GET', url)
+            if data.ok:
+                price_usd = data.json()['data']['market_data']['price_usd']
+                price_btc = data.json()['data']['market_data']['price_btc']
+                name = data.json()['data']['name']
+                print('Criptodivisa seleccionada: {}'.format(name))
+                print('El precio en BTC es de {btc}, y en USD {usd}'.format(
+                    btc=price_btc,
+                    usd=price_usd
+                ))
+                cantidad = int(input('Introduce la cantidad que deseas convertir: '))
+                total_btc = cantidad * price_btc
+                total_usd = cantidad * price_usd
+                print('{cantidad} {asset} a BTC = {total_btc}'.format(
+                    cantidad=cantidad,
+                    asset=asset,
+                    total_btc=total_btc
+                ))
+                print('{cantidad} {asset} a USD = {total_usd}'.format(
+                    cantidad=cantidad,
+                    asset=asset,
+                    total_usd=total_usd
+                ))
+                input('Presiona una tecla para continuar')
+            else:
+                print('No se encuentra el simbolo seleccionado')
+                input('Presiona una tecla para continuar')
+        elif menu == 2:
+            limit = input('¿Cuántos quieres mostrar? (máximo 500)')
+            if is_int(limit):
+                url = 'https://data.messari.io/api/v2/assets?fields=name,symbol&limit={}'.format(
+                    limit
+                )
+                data = requests.request('GET', url)
+                if data.ok:
+                    for key in data.json()['data']:
+                        print('Nombre: {name}:  -- Símbolo: {symbol}'.format(
+                            name=key['name'],
+                            symbol=key['symbol']
+                        ))
+                else:
+                    print('No se encuentra el simbolo seleccionado')
+                    input('Presiona una tecla para continuar')
+            else:
+                print('Debes indicar un número entero')
+        elif menu == 0:
+            print('Adiós')
+        else:
+            print('Opción no disponible')
