5fb31b93612cd29220416a22991144fc8b619495
jv3ga <joel.v3ga@gmail.com>
2022-03-05 23:22:24 +0100

    feat: funcionando la conversión

--- api.py
+++ api.py
@@ -1,13 +1,6 @@
 import requests
-import json
 
-# url = 'https://data.messari.io/api/v1/assets?fields=id,name,slug,symbol,metrics/market_data/price_usd'
-asset = 'BTC'
-url = 'https://data.messari.io/api/v1/assets/{}/metrics'.format(asset)
-data = requests.request('GET', url)
-print(data.json()['data']['market_data']['price_usd'])
-# menu = -1
-menu = 0
+menu = -1
 while menu != 0:
     print('Seleccione una opcion: ')
     print('1 - Convertir precio de criptomoneda')
@@ -15,7 +8,28 @@ while menu != 0:
     print('0 - Salir')
     menu = int(input('Opción seleccionada: '))
     if menu == 1:
-        print('Introduce la moneda que quieras convertir')
+        asset = input('Introduce la moneda que quieras convertir: ').upper()
+        url = 'https://data.messari.io/api/v1/assets/{}/metrics'.format(asset)
+        data = requests.request('GET', url)
+        price_usd = data.json()['data']['market_data']['price_usd']
+        price_btc = data.json()['data']['market_data']['price_btc']
+        print('El precio en BTC es de {btc}, y en USD {usd}'.format(
+            btc=price_btc,
+            usd=price_usd
+        ))
+        cantidad = int(input('Introduce la cantidad que deseas convertir: '))
+        total_btc = cantidad * price_btc
+        total_usd = cantidad * price_usd
+        print('{cantidad} {asset} a BTC = {total_btc}'.format(
+            cantidad=cantidad,
+            asset=asset,
+            total_btc=total_btc
+        ))
+        print('{cantidad} {asset} a USD = {total_usd}'.format(
+            cantidad=cantidad,
+            asset=asset,
+            total_usd=total_usd
+        ))
     elif menu == 2:
         print('2')
     elif menu == 0:
